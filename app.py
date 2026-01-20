41f7038324163f3ea30e2e6aa8df9cd30ad84e52
dahernandez-cmyk <dahernandez@coninsa.co>
2026-01-20 07:40:25 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+import pyodbc
+
+# --- CONFIGURACIÓN DE PÁGINA ---
+st.set_page_config(page_title="Extractor de Datos Coninsa", page_icon="📊")
+st.title("📥 Descarga de datos - Azure SQL")
+
+# --- CREDENCIALES (Usa las de tu imagen) ---
+server = st.secrets["server"]
+database = st.secrets["database"]
+username = st.secrets["username"]
+password = st.secrets["password"]
+driver = '{ODBC Driver 17 for SQL Server}' #Usar 18 para cuando este localmente
+
+# --- FUNCIÓN PARA CONECTAR Y EXTRAER ---
+def obtener_datos(query):
+    try:
+        # Solo si usas el Driver 18
+        conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password};Encrypt=yes;TrustServerCertificate=yes'
+        #conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+        # Usamos pandas para leer directamente con la conexión
+        with pyodbc.connect(conn_str) as conn:
+            df = pd.read_sql(query, conn)
+        return df
+    except Exception as e:
+        st.error(f"Error de conexión: {e}")
+        return None
+
+# --- INTERFAZ DE USUARIO ---
+nombre_tabla = st.text_input("Escribe el nombre de la tabla:", value="FNX_GTH.Empleados")
+
+if st.button("Consultar Información"):
+    with st.spinner("Consultando base de datos..."):
+        query = f"SELECT * FROM {nombre_tabla}"
+        df_resultado = obtener_datos(query)
+
+        if df_resultado is not None:
+            st.success(f"¡Éxito! Se encontraron {len(df_resultado)} filas.")
+            
+            # Mostrar vista previa
+            st.dataframe(df_resultado.head(10))
+
+            # --- BOTÓN DE DESCARGA ---
+            # Convertimos el DataFrame a CSV
+            csv = df_resultado.to_csv(index=False).encode('utf-8')
+            
+            st.download_button(
+                label="Confirmar y Descargar CSV",
+                data=csv,
+                file_name=f'datos_{nombre_tabla}.csv',
+                mime='text/csv',
+            )
